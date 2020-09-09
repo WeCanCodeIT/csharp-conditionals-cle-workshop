51c7563a9dbad77c8763fbc0cf145a035b8e83b1
Jen E <jenevans3ten@gmail.com>
2020-09-09 10:16:53 -0400

    starter conditionals workshop

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp_conditionals_workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Make Some Decisions!");
+
+            Console.WriteLine("What is your name?  If your name is the same as mine, I will let you know.");
+
+            Console.WriteLine("Is it hot or cold outside?");
+
+            Console.WriteLine("Enter a number.  I will tell you if it is larger, smaller, or equal to 10'");
+
+            Console.WriteLine("What kind of pet do you have?  Answer 'none' if you don't have any pets.");
+        }
+    }
+}
