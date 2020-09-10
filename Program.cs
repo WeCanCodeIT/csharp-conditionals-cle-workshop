2290ba2e823a27b9757c3fe6a9f60038d9ab86af
Jen E <jenevans3ten@gmail.com>
2020-09-10 10:49:00 -0400

    added code for if and if/else

--- Program.cs
+++ Program.cs
@@ -9,12 +9,33 @@ namespace csharp_conditionals_workshop
             Console.WriteLine("Let's Make Some Decisions!");
 
             Console.WriteLine("What is your name?  If your name is the same as mine, I will let you know.");
+            // create variable to save my name
+            string myName = "jen";
+
+            // read user's response
+            string userName = Console.ReadLine();
+
+            // compare my name to user's name
+            // if they are the same, print message to screen
+            if (myName.Equals(userName.ToLower()))
+            {
+                Console.WriteLine("Hey, your name is the same as mine!");
+            }
 
             Console.WriteLine("Is it hot or cold outside?");
+            string outsideTemperature = Console.ReadLine();
+            if (outsideTemperature.ToLower().Equals("hot"))
+            {
+                Console.WriteLine("Yes, it is hot outside");
+            }
+            else
+            {
+                Console.WriteLine("OK, it's not hot outside");
+            }
 
-            Console.WriteLine("Enter a number.  I will tell you if it is larger, smaller, or equal to 10'");
+            // Console.WriteLine("Enter a number.  I will tell you if it is larger, smaller, or equal to 10'");
 
-            Console.WriteLine("What kind of pet do you have?  Answer 'none' if you don't have any pets.");
+            // Console.WriteLine("What kind of pet do you have?  Answer 'none' if you don't have any pets.");
         }
     }
 }
